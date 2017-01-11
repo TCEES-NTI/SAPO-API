c02d5313659084ee6f0820e974b3e13ea7a7fe8d
Luciano Pellacani Franca <lucianopf@outlook.com>
2017-01-11 13:37:47 +0000

    adding CORS support

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api', require('./app/routes/default'))
 
 populateDB()
