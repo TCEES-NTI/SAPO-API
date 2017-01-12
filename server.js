439274ba9c0fb65222af3983a90bc5cbfd8732f4
Luciano Pellacani Franca <lucianopf@outlook.com>
2017-01-12 11:35:04 +0000

    adding support for the right headers and cors

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
+
   next();
 });
 
