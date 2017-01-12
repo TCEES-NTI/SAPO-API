0a49368da002f2ce00e62828b225f23c8c16d1b7
Luciano Pellacani Franca <lucianopf@outlook.com>
2017-01-12 12:05:01 +0000

    handling pre-flight requests

--- server.js
+++ server.js
@@ -14,10 +14,14 @@ app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
-
-  next();
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  if ('OPTIONS' == req.method) {
+      res.send(200);
+  }
+  else {
+    next();
+  }
 });
 
 app.use('/api', require('./app/routes/default'))
